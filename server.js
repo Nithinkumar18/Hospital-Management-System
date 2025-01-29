ee0925bbb22e60d4175bfeaa9ba72ca514fb11cf
Nithin <vooturinithinkumar@gmail.com>
2025-01-29 18:36:34 +0530

    initialize server

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const connectionTime = require('./src/utilities/utility');
 const app = express();
+const userRoutes = require('./src/routes/userRoutes');
+
+
 
 
 dotenv.config();
+
+app.use(express.json());
+app.use('/user/api',userRoutes);
 mongoose.connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
