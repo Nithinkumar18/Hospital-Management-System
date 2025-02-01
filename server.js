2a2f5f917fb646051d8fe93f72ae8d02439afbc3
Nithin <vooturinithinkumar@gmail.com>
2025-02-01 12:53:42 +0530

    add routes to server

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const mongoose = require('mongoose');
 const connectionTime = require('./src/utilities/utility');
 const app = express();
 const userRoutes = require('./src/routes/userRoutes');
-
-
+const patientRoutes = require('./src/routes/patientRoutes');
+const doctorRoutes = require('./src/routes/doctorRoutes');
+const appointmentRoutes = require('./src/routes/appointmentRoutes');
 
 
 dotenv.config();
 
 app.use(express.json());
 app.use('/user/api',userRoutes);
+app.use('/patient/api',patientRoutes);
+app.use('/doctor/api',doctorRoutes);
+app.use('/appointment/api',appointmentRoutes);
 mongoose.connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
